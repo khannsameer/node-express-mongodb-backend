50f0dd7daf2046d64b3108441eab05525039fa9c
khannsameer <sk8717108@gmail.com>
2025-09-27 22:23:41 +0530

    Update app.js and math.js

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-const add = require("./math"); //commonjs
+// const add = require("./math"); //commonjs
+
+const { add, sub, div, mult } = require("./math");
 
 console.log(add(5, 10));
+console.log(sub(50, 10));
+console.log(div(5, 5));
+console.log(mult(5, 10));
--- math.js
+++ math.js
@@ -2,4 +2,21 @@ const add = (a, b) => {
   return a + b;
 };
 
-module.exports = add;
+const sub = (a, b) => {
+  return a - b;
+};
+
+const div = (a, b) => {
+  return a / b;
+};
+
+const mult = (a, b) => {
+  return a * b;
+};
+
+// module.exports.add = add;
+// module.exports.sub = sub;
+// module.exports.div = div;
+// module.exports.mult = mult;
+
+module.exports = { add, sub, div, mult };
