051eab5f42854efa8163a1055dc34cdce10e7db2
khannsameer <sk8717108@gmail.com>
2026-01-08 22:50:08 +0530

    feat(esmodule): add ES module example with math utilities
    - added app.js and math.js using ES module syntax
    - updated package.json for module support

---
+++ esModule/app.js
@@ -0,0 +1,11 @@
+// const add = require("./math"); //commonjs
+
+// const { add, sub, div, mult } = require("./math");
+
+// import multiply from "./math.js";   // default import
+import { add, sub, div, mult } from "./math.js";
+
+console.log(add(5, 10));
+console.log(sub(50, 10));
+console.log(div(5, 5));
+console.log(mult(30, 10));
---
+++ esModule/math.js
@@ -0,0 +1,18 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const sub = (a, b) => {
+  return a - b;
+};
+
+const div = (a, b) => {
+  return a / b;
+};
+const mult = (a, b) => {
+  return a * b;
+};
+
+// export default mult;
+
+export { add, sub, div, mult };
