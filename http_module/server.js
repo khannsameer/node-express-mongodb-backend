a5b0f9a68ba43304573a2366f25b2ec26a8700d4
khannsameer <sk8717108@gmail.com>
2026-01-08 20:52:10 +0530

    feat: add basic HTTP server module

---
+++ http_module/server.js
@@ -0,0 +1,26 @@
+const http = require("http");
+
+//web server
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Welcome to home page");
+    res.end(); //send the data
+  }
+
+  if (req.url === "/source-code") {
+    res.write("Source code is available");
+    res.end(); //send the data
+  }
+
+  if (req.url === "/contact") {
+    res.setHeader("Content-Type", "text/plain");
+    res.write("Contact us");
+    res.end(); //send the data
+  }
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});
