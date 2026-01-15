1d594e66117fa09c3b1846a0cb2d0fb94fe95e6f
khannsameer <sk8717108@gmail.com>
2026-01-15 20:30:36 +0530

    update express app config and env example

--- express/app.js
+++ express/app.js
@@ -2,11 +2,13 @@ import express from "express";
 
 const app = express();
 
-const PORT = 3000;
+// const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
