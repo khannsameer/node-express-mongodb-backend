f63f5fd93fd992c9b4b2eaa9e6fbf46c73b5aea1
khannsameer <sk8717108@gmail.com>
2026-01-17 20:37:04 +0530

    update express app.js file

--- express/app.js
+++ express/app.js
@@ -4,12 +4,10 @@ import path from "path";
 
 const app = express();
 
-//absolute path
-const staticPath = path.join(import.meta.dirname, "public");
-
-app.use("/public", express.static(staticPath));
-
-//./public --> relative path
+//Node.js (14.8+) ypu can use top-level await without needing to wrap it in an async function
+const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+const json = await response.json();
+console.log(json);
 
 // const PORT = 3000;
 
@@ -32,9 +30,14 @@ app.use("/public", express.static(staticPath));
 //   console.log(import.meta.dirname);
 //   console.log(import.meta.url); //for filename
 
+//./public --> relative path
+
+//absolute path
+const staticPath = path.join(import.meta.dirname, "public");
+app.use("/public", express.static(staticPath));
+
 app.get("/", (req, res) => {
   const homePagePath = path.join(import.meta.dirname, "public", "index.html");
-
   res.sendFile(homePagePath);
 });
 
