2c12687c3560990c7131c3411bd38da236238536
khannsameer <sk8717108@gmail.com>
2026-01-19 23:51:36 +0530

    modified in app.js file in express folder

--- express/app.js
+++ express/app.js
@@ -5,9 +5,9 @@ import path from "path";
 const app = express();
 
 //Node.js (14.8+) ypu can use top-level await without needing to wrap it in an async function
-const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+/* const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
 const json = await response.json();
-console.log(json);
+console.log(json); */
 
 // const PORT = 3000;
 
@@ -36,7 +36,21 @@ console.log(json);
 const staticPath = path.join(import.meta.dirname, "public");
 app.use(express.static(staticPath));
 
-app.get("/product", (req, res) => {
+app.use(express.urlencoded({ extended: true })); //to access data from post request.to parse the body request
+
+// form get method
+// app.get("/contact", (req, res) => {
+//   console.log(req.query);
+//   res.redirect("/");
+// });
+
+//post method
+app.post("/contact", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+/* app.get("/product", (req, res) => {
   console.log(req.query);
   // res.send(`<h1>user search for product ${req.query.search} mobile</h1>`);
   res.send(
@@ -58,7 +72,7 @@ app.get("/profile/:username/article/:slug", (req, res) => {
   console.log(req.params);
   const formatedSlug = req.params.slug.replace(/-/g, " ");
   res.send(`<h1>Article ${req.params.username} by ${formatedSlug}</h1>`);
-});
+}); */
 
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
