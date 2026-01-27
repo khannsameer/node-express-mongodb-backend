e841fd0da69f412bcab493e1c1fd2b2046cbb211
khannsameer <sk8717108@gmail.com>
2026-01-27 21:30:20 +0530

    modified:   express/expressURL/routers/auth.routes.js

--- express/expressURL/routers/auth.routes.js
+++ express/expressURL/routers/auth.routes.js
@@ -1,10 +1,10 @@
-import { Router } from "express";
-import * as authContollers from "../controllers/auth.contoller.js";
+// import { Router } from "express";
+// import * as authContollers from "../controllers/auth.contoller.js";
 
-const router = Router();
+// const router = Router();
 
-//someone hit this route the call the controller too display what the content
-router.get("/register", authContollers.getRegisterPage);
-router.get("/login", authContollers.getLoginPage);
+// //someone hit this route the call the controller too display what the content
+// router.get("/register", authContollers.getRegisterPage);
+// router.get("/login", authContollers.getLoginPage);
 
-export const authRoutes = router;
+// export const authRoutes = router;
