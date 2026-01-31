e66104e45ca380fd8a0ce1748c90842f33b46aa2
khannsameer <sk8717108@gmail.com>
2026-01-31 18:11:53 +0530

    modified:   mongoDB/aggregate/test.js

--- mongoDB/aggregate/test.js
+++ mongoDB/aggregate/test.js
@@ -6,3 +6,22 @@ db.products.aggregate([
     },
   },
 ]);
+
+db.products.aggregate([
+  {
+    $match: {
+      _id: "6978c827fd1c33d923f5d7f8",
+    },
+  },
+]);
+
+db.products.aggregate([
+  { $match: { price: { $gt: 1200 } } },
+  {
+    $project: {
+      price: 1,
+      _id: 0,
+      discountPrice: { $multiply: ["$price", 0.8] },
+    },
+  },
+]);
